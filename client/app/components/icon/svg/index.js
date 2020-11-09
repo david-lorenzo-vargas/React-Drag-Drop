2bad3fb3059b057bb13efe0223dc4eca06e15524
unknown <dlorenzo.05@gmail.com>
2020-11-09 15:41:49 +0100

    failure svg icon exported

--- client/app/components/icon/svg/index.js
+++ client/app/components/icon/svg/index.js
@@ -2,10 +2,12 @@ import excel from './excel.svg';
 import jpeg from './jpeg.svg';
 import pdf from './pdf.svg';
 import word from './word.svg';
+import failure from './failure.svg';
 
 export {
   excel,
   jpeg,
   pdf,
   word,
+  failure,
 };
